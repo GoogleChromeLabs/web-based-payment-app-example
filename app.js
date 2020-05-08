c254b80597078580156908bd317a875b6e0b3784
Eiji Kitamura <agektmr@google.com>
2020-05-08 17:18:56 +0900

    Fix license

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 
 let express = require('express');
 let app = express();
 
+// All of our paths have the Link header.
+app.use(function(req, res, next) {
+  res.status(200).links({
+    'payment-method-manifest':
+        'https://bobpay.xyz/pay/payment-manifest.json',
+    });
+    return next();
+});
 // We are mostly a static website.
-app.use(express.static('app'));
+app.use(express.static('public'));
 
 /**
  * Starts the server.
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,77 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 
-gulp.task('generate-service-worker', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = 'app';
 
-  swPrecache.write(`${rootDir}/service-worker.js`, {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
-    stripPrefix: rootDir
-  }, callback);
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('public/less/new-age.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('public/css/new-age.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('public/js/new-age.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'public'
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('public/less/*.less', ['less']);
+    gulp.watch('public/css/*.css', ['minify-css']);
+    gulp.watch('public/js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('public/*.html', browserSync.reload);
+    gulp.watch('public/js/**/*.js', browserSync.reload);
 });
