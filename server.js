142fef7b4c810045263c52ebd522733ed993d2aa
Eiji Kitamura <agektmr@google.com>
2021-11-26 14:18:11 +0900

    Tweaks for bobbucks.dev

--- server.js
+++ server.js
@@ -113,6 +113,6 @@ app.get('/favicon.ico', (req, res) => {
 app.use('/user', user);
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT || 8080, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
